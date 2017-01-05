157f9b688a8073ad415bf970de475b3abe8a0db6
regireed89 <regireed89@gmail.com>
2017-01-05 17:56:53 -0600

    ex

--- Exercises/Exercises/Program.cs
+++ Exercises/Exercises/Program.cs
@@ -10,33 +10,46 @@ namespace Exercises
     {
         static void Main(string[] args)
         {
-            List<string> Fizz = new List<string>();
-            for (int i = 0; i <= 100; i++)
-            {
-                string a = i.ToString();
-                Fizz.Add(a);
-                if (i % 3 == 0 && i % 5 == 0)
-                {
-                    a = "FizzBuzz";
-                    Console.WriteLine(a);                   
-                }
-                else if (i % 3 == 0)
-                {
-                    a = "Fizz";
-                    Console.WriteLine(a);
-                }
-                else if (i % 5 == 0)
-                {
-                    a = "Buzz";
-                    Console.WriteLine(a);
-                }
-                else
-                {
-                    Console.WriteLine(i);
-                }
-                Console.ReadLine();
-            }
+            //List<string> Fizz = new List<string>();
+            //for (int i = 0; i <= 100; i++)
+            //{
+
+            //    string a = i.ToString();
+            //    Fizz.Add(a);
+            //    if (i % 3 == 0 && i % 5 == 0)
+            //    {
+            //        a = "FizzBuzz";
+            //        Console.WriteLine(a);
+            //    }
+            //    else if (i % 3 == 0)
+            //    {
+            //        a = "Fizz";
+            //        Console.WriteLine(a);
+            //    }
+            //    else if (i % 5 == 0)
+            //    {
+            //        a = "Buzz";
+            //        Console.WriteLine(a);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //    Console.ReadLine();
+            //}
+            rps();
         }
+
+
+        public string rps()
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.ReadLine();
+
+
+        }
+
     }
 }
 
