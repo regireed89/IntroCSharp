ed2e3d1906749179d247308194fe74c8a4b35416
regireed89 <regireed89@gmail.com>
2017-01-05 16:10:40 -0600

    ex

--- Exercises/Exercises/Program.cs
+++ Exercises/Exercises/Program.cs
@@ -10,8 +10,38 @@ namespace Exercises
     {
         static void Main(string[] args)
         {
-            
-
+            List<string> Fizz = new List<string>();
+            for (int i = 0; i <= 100; i++)
+            {
+                string a = i.ToString();
+                Fizz.Add(a);
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    a = "FizzBuzz";
+                    Console.WriteLine(a);                   
+                }
+                else if (i % 3 == 0)
+                {
+                    a = "Fizz";
+                    Console.WriteLine(a);
+                }
+                else if (i % 5 == 0)
+                {
+                    a = "Buzz";
+                    Console.WriteLine(a);
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
+
+
+
+
+
+
