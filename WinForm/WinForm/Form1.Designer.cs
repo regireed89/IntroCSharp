be842e14099efa3cc0f9db91cd1540c57c40169c
regireed89 <regireed89@gmail.com>
2017-01-10 13:16:10 -0600

    rps

--- WinForm/WinForm/Form1.Designer.cs
+++ WinForm/WinForm/Form1.Designer.cs
@@ -85,7 +85,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(138, 31);
             this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
+            this.button1.Text = "GO!!!";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
