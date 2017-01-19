1c1446f063020a6b52f15ed4995f6c8bbc170901
regireed89 <regireed89@gmail.com>
2017-01-19 17:59:53 -0600

    TrafficLight

---
+++ TrafficLightFsm/TrafficLightFsm/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Timers;
+
+namespace TrafficLightFSM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+       public class StopWatch
+        {
+            public StopWatch(){ }
+        }
+        public class FSM<T>
+        {
+            public FSM() { }
+            public void Start(){ }
+            public void Update() { }
+            public void Feed() { }
+        }
+
+        public class State
+        {
+            public State() { }
+         enum states
+            {
+                Init,
+                Red,
+                Green,
+                Yellow,
+                Exit
+            };
+
+        }
+
+        public class Transition
+        {
+            public Transition() { }
+
+        }
+
+
+
+    }
+}
