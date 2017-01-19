fb0350d9e41a2705f62b6d544fa3c4fd014e459e
regireed89 <regireed89@gmail.com>
2017-01-19 12:54:31 -0600

    Serialization

---
+++ Serialization/Serialization/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Serialization
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.name = new System.Windows.Forms.TextBox();
+            this.ID = new System.Windows.Forms.TextBox();
+            this.Age = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SaveData = new System.Windows.Forms.Button();
+            this.LoadLast = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Age)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(42, 49);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(125, 20);
+            this.name.TabIndex = 0;
+            // 
+            // ID
+            // 
+            this.ID.Location = new System.Drawing.Point(42, 170);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(124, 20);
+            this.ID.TabIndex = 1;
+            // 
+            // Age
+            // 
+            this.Age.Location = new System.Drawing.Point(42, 112);
+            this.Age.Name = "Age";
+            this.Age.Size = new System.Drawing.Size(124, 20);
+            this.Age.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Age";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "ID Number";
+            // 
+            // SaveData
+            // 
+            this.SaveData.Location = new System.Drawing.Point(38, 240);
+            this.SaveData.Name = "SaveData";
+            this.SaveData.Size = new System.Drawing.Size(128, 39);
+            this.SaveData.TabIndex = 6;
+            this.SaveData.Text = "Save";
+            this.SaveData.UseVisualStyleBackColor = true;
+            this.SaveData.Click += new System.EventHandler(this.SaveData_Click);
+            // 
+            // LoadLast
+            // 
+            this.LoadLast.Location = new System.Drawing.Point(196, 240);
+            this.LoadLast.Name = "LoadLast";
+            this.LoadLast.Size = new System.Drawing.Size(125, 38);
+            this.LoadLast.TabIndex = 7;
+            this.LoadLast.Text = "Load";
+            this.LoadLast.UseVisualStyleBackColor = true;
+            this.LoadLast.Click += new System.EventHandler(this.LoadLast_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 355);
+            this.Controls.Add(this.LoadLast);
+            this.Controls.Add(this.SaveData);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Age);
+            this.Controls.Add(this.ID);
+            this.Controls.Add(this.name);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Age)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.TextBox ID;
+        private System.Windows.Forms.NumericUpDown Age;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button SaveData;
+        private System.Windows.Forms.Button LoadLast;
+    }
+}
+
---
+++ Serialization/Serialization/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace Serialization
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Student
+        {
+            private string mName;
+            private int mAge;
+            private string mID;
+            public Student() { }
+            public Student(string mName, int mAge, string mID)
+            {
+
+            }
+            public string Name
+            {
+                get
+                {
+                    return mName;
+                }
+                set
+                {
+                    mName = value;
+                }
+            }
+            public int Age
+            {
+                get
+                {
+                    return mAge;
+                }
+                set
+                {
+                    mAge = value;
+                }
+            }
+            public string ID
+            {
+                get
+                {
+                    return mID;
+                }
+                set
+                {
+                    mID = value;
+                }
+            }
+        }
+
+        class Serialization<T>
+        {
+            /// <summary>
+            /// We invoke this function when have the data we want to serialize ready. The arguments we pass in to
+            /// the functiom call tell the application where to save the data which by default is the project 
+            /// directory two files deep in a folder called SavedFiles and inside that folder we will find a file
+            /// with the name that matches the data stored inside the string passed into the function call
+            /// Once that file has been found we then take the data passed into to function and write it to a said file
+            /// </summary>
+            /// <param name="fileName"></param>
+            /// <param name="data"></param>
+            public static void Serialize(string fileName, T data)
+            {
+                //creates a new XMLS Seualizer object with an argument of 
+                //the type of the data passed in
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                //Creates a new TextWritter object and assigns it a StreamWritter 
+                //object that takes in argument of the file path
+                TextWriter writter = new StreamWriter(@"..\..\SavedFiles" + fileName + ".xml");
+                //Invoke the Serialize function from the XmlSerialize class and pass in 
+                //the TextWritter object created and the data we are trying to write to the file
+                serializer.Serialize(writter, data);
+                //Closes the file we were using to write our data
+                writter.Close();
+            }
+            /// <summary>
+            /// We invoke this function when we are trying load some data back into our application that has all ready been saved.
+            /// This function will break if you are trying to find a file that does not exist. We specify the file we are looking
+            /// for by the data stored in the string passed into the function call.
+            /// </summary>
+            /// <param name="fileName"></param>
+            /// <returns></returns>
+            public static T Deserialize(string fileName)
+            {
+                //Creates a new Generic object we will use to hold the data we are trying to return 
+                T data;
+                //creates a new XmlSerializer object with an argument of the type of generic object we are trying to create 
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                //creates a new textreader pbject with a child object of the class eith the argument of a string that is the 
+                //file directory of the file we are trying to get the data from 
+                TextReader reader = new StreamReader(@"..\..\SavedFiles\" + fileName + ".xml");
+                //invokes the deserialize function from the XmlSerializer class and casts the return as the generis type and 
+                //passes in the TextReader object as an argument into the function call
+                data = (T)serializer.Deserialize(reader);
+                //Closes the reader file 
+                reader.Close();
+                //Returns our generic object
+                return data;
+            }
+            
+        }
+        /// <summary>
+        /// Function that is invoked when the SaveData button is click by the user.
+        /// This function will create a new student with the information provided by the user from the various
+        /// fields on the winform
+        /// Once we have the student created we will invoke the Serialize function from the DataSerialization class.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SaveData_Click(object sender, EventArgs e)
+        {
+            //Creates a student and invoke the constructor with the arguments being the data from the various fields
+            //in the winform
+            Student curStudent = new Student(this.name.Text, (int)this.Age.Value, this.ID.Text);
+            //Invokes the Serialize function from the DataSerialization class and gives it the arguments of the string 
+            //"Student" and the newly created student
+            Serialization<Student>.Serialize("Student", curStudent);
+        }
+        /// <summary>
+        /// Function that is invoked when he LoadLast button is clicked by the user.
+        /// This function will invoke the Deseriaize function to create a new student and take that data and assign 
+        /// the data from that stsudent to the various fields
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void LoadLast_Click(object sender, EventArgs e)
+        {
+            //Creates a new student with the information returned from the Deserialize function
+            Student lastStudent = Serialization<Student>.Deserialize("Student");
+            this.name.Text = lastStudent.Name;
+            this.Age.Value = lastStudent.Age;
+            this.ID.Text = lastStudent.ID;
+        }
+    }
+}
