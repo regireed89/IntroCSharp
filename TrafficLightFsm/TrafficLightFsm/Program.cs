10633983a294acfb9ebb55cc4ff0311b445f32f8
regireed89 <regireed89@gmail.com>
2017-01-25 01:52:02 -0600

    traffic

--- TrafficLightFsm/TrafficLightFsm/Program.cs
+++ TrafficLightFsm/TrafficLightFsm/Program.cs
@@ -8,44 +8,57 @@ using System.Timers;
 
 namespace TrafficLightFSM
 {
+
+    public enum LightState
+    {
+        Init = 0,
+        Red = 1,
+        Green = 2,
+        Yellow = 3,
+        Exit = 9000
+    };
+
+    
+    public class State
+    {
+        public State() { }
+        public State(Enum e)
+        {
+            name = e.ToString();
+        }
+        public string name;
+        public delegate void OnEnter();
+        public delegate void OnExit();
+        public OnEnter onEnter;
+        public OnExit onExit;
+
+        public void AddEnterFunction(Delegate d)
+        {
+            onEnter += d as OnEnter;
+        }
+    }
+
+
+    public class FSM<T>
+    {
+        public FSM() { }
+        public void ChangeState(State state) { }
+        public bool AddState(State state) { return false; }
+
+        public bool AddTransition<V>() { return false; }
+
+        public void Start() { }
+        public void Update() { }
+        public void Feed() { }
+    }
+
+
+    
+
     class Program
     {
         static void Main(string[] args)
         {
         }
-       public class StopWatch
-        {
-            public StopWatch(){ }
-        }
-        public class FSM<T>
-        {
-            public FSM() { }
-            public void Start(){ }
-            public void Update() { }
-            public void Feed() { }
-        }
-
-        public class State
-        {
-            public State() { }
-         enum states
-            {
-                Init = 0,
-                Red = 1,
-                Green = 2,
-                Yellow = 3,
-                Exit = 9000
-            };
-
-        }
-
-        public class Transition
-        {
-            public Transition() { }
-
-        }
-
-
-
     }
 }
