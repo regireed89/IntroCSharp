71dbca5d0cff20b717233a5196c2a54bd0885a69
regireed89 <regireed89@gmail.com>
2017-01-25 16:33:06 -0600

    1.25.17_assignment

---
+++ 1-25_morningAssignment/1-25_morningAssignment/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_25_morningAssignment
+{
+    class Program
+    {
+        static public char Invert(char s)
+        {
+            return ((s == '0') ? '1' : '0');
+        }
+
+        public class Gene
+        {
+            public Gene(string s)
+            {
+                if (s.Length != 8)
+                    throw new Exception("too big or too small... must be 8 long");
+                value = s;
+                tail = value.Substring(4);
+                head = value.Remove(4, 4);
+            }
+            string value;
+            string tail;
+            string head;
+            public override string ToString()
+            {
+                return value;
+            }
+            public string Head
+            {
+                get
+                {
+                    return head;
+                }
+            }
+
+            public string Tail
+            {
+                get
+                {
+                    return tail;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Gene gene = new Gene("11111111");
+            string back = gene.Tail;
+            string front = gene.Head;
+            for(int i = 0; i < back.Length; i++)
+            {
+                foreach (char c in back)
+                {
+                    char newfront = Program.Invert(c);
+                                     
+                }
+            } 
+        }
+    }
+    //class Program
+    //{
+    //    //public string Strangs(string a)
+    //    //{
+    //    //    a =  "1 1 1 1  1 1 1 1";
+    //    //    a.Substring
+    //    //    string[] ar = new string[255];
+
+
+    //    //    return a;
+    //    //}
+    //    static void Main(string[] args)
+    //    {
+
+    //    }
+
+    //}
+}
