47e7014af660449709b1fb852dd9693260cb3bb3
regireed89 <regireed89@gmail.com>
2017-01-26 16:09:00 -0600

    1.26.17

---
+++ 1.26.17assignment/1.26.17assignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._26._17assignment
+{
+    public delegate void Handler();
+    class This
+    {
+        Handler onShout;
+        This() {  }
+        public void AddFunciton(Delegate func)
+        {
+
+
+        }
+      
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
