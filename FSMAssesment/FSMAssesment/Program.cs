341db04a1742c412ff5d64d52b6116296c492b9c
regireed89 <regireed89@gmail.com>
2017-01-26 09:43:11 -0600

    asses

---
+++ FSMAssesment/FSMAssesment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSMAssesment
+{
+    public class FSM<T>
+    {
+        FSM() { }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
