4ef7e77c5f27a250485d366291a81fff8e36840c
regireed89 <regireed89@gmail.com>
2017-01-27 17:58:45 -0600

    interface

---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+
+    class Ninja : IDamager, IDamageable
+    {
+        public Ninja(string n, int h, int d)
+        {
+            n = m_name;
+            h = m_health;
+            d = m_damage;
+        }
+        public int m_health;
+        public int m_damage;
+        public string m_name;
+
+        public string Name
+        {
+            get { return m_name; }
+            set { m_name = value; }
+        }
+        public int Health
+        {
+            get { return m_health; }
+            set { m_health = value; }
+        }
+        public int Damage
+        {
+            get { return m_damage; }
+            set { m_damage = value; }
+        }
+        
+
+    }
+    class Zambie : IDamager, IDamageable
+    {
+        public Zambie(string n, int h, int d)
+        {
+            n = m_name;
+            h = m_health;
+            d = m_damage;
+        }
+        public int m_health;
+        public int m_damage;
+        public string m_name;
+        public string Name
+        {
+            get { return m_name; }
+            set { m_name = value; }
+        }
+        public int Health
+        {
+            get { return m_health; }
+            set { m_health = value; }
+        }
+        public int Damage
+        {
+            get { return m_damage; }
+            set { m_damage = value; }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+            }
+        }
+    }
+}
