3d71e72fdef7aacce020f02377a15cfaf32ce25a
regireed89 <regireed89@gmail.com>
2017-02-03 01:54:33 -0600

    lights
    change lights using switch statements

--- TrafficLightFsm/TrafficLightFsm/Program.cs
+++ TrafficLightFsm/TrafficLightFsm/Program.cs
@@ -18,60 +18,66 @@ namespace TrafficLightFSM
         Exit = 4
     };
 
-
-    public class State
-    {
-        public State() { }
-        public State(Enum e)
-        {
-            name = e.ToString();
-        }
-        public string name;
-        public delegate void OnEnter();
-        public delegate void OnExit();
-        public OnEnter onEnter;
-        public OnExit onExit;
-
-        public void AddEnterFunction(Delegate d)
-        {
-            onEnter += d as OnEnter;
-        }
-    }
-
-
     public class FSM
     {
-        LightState currentstate;
-
+       // LightState currentstate;
+        int x = 0;
         public FSM() { }
-        public LightState ChangeState(LightState currentlight, LightState next)
-        {
-            currentlight = next;
-            currentstate = currentlight;
-            return currentstate;
-        }
-        public bool AddState(LightState state)
-        {
-            return false;
-        }         
-
-        public bool IsTransitionValid()
-        {
-            return false;
-        }
         public void Start()
-        {           
-            currentstate = LightState.Init;
+        {
+            //currentstate = LightState.Init;
+
+            while (true)
+            {
+                x = Convert.ToInt32(Console.ReadLine());
+                switch (x)
+                {
+                    case 1:
+                        Console.WriteLine("i am green");
+                        break;
+                    case 2:
+                        Console.WriteLine("i am yellow");
+                        break;
+                    case 3:
+                        Console.WriteLine("i am red");
+                        break;
+                    case 4:
+                        Console.WriteLine("i am leaving");
+                        break;
+
+                }
+            }
         }
 
-        public void Update()
-        {
-          
-        }
+        //public void ToGreen()
+        //{
+        //    currentstate = LightState.Green;
+        //    Console.WriteLine("I am green");
+        //}
+
+        //public void ToYellow()
+        //{
+        //    currentstate = LightState.Yellow;
+        //    Console.WriteLine("I am yellow");
+        //}
+
+        //public void ToRed()
+        //{
+        //    currentstate = LightState.Red;
+        //    Console.WriteLine("I am red");
+        //}
+
+        //public void ToExit()
+        //{
+        //    currentstate = LightState.Exit;
+        //    Console.WriteLine("I am leaving");
+        //}
+
+
     }
-    class Program 
-    { 
-        static void Main(string[] args) 
+    class Program
+    {
+        static void Main(string[] args)
         {
             FSM fsm = new FSM();
             fsm.Start();
